93ae1220993c022d6b6ab505808056b79af83c0d
AnkeshGautam <53037369+AnkeshGautam@users.noreply.github.com>
2019-07-20 16:11:31 +0530

    Add files via upload

---
+++ JavaScript/JS/manageClock.js
@@ -0,0 +1,27 @@
+var startButton = document.querySelector('#start');
+var stopButton = document.getElementById("stop");
+
+startButton.addEventListener('click', startClock);
+stopButton.addEventListener('click', stopClock);
+
+var minute = 0;
+var second = 0;
+
+var intervalID = null;
+
+function startClock() {
+
+    intervalID = setInterval(function () {
+        if (second == 60) {
+            second = 0;
+            minute++;
+            document.getElementById("min").innerHTML = minute;
+        }
+        second++;
+        document.getElementById("sec").innerHTML = second;
+    }, 1000);
+}
+
+function stopClock() {
+    clearInterval(intervalID);
+}
