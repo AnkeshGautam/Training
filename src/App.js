106e3dccb04b13c6806a3a093c744098b86b9d7b
AnkeshGautam <53037369+AnkeshGautam@users.noreply.github.com>
2019-08-02 15:23:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import About from './about/About';
 import Albums from './albums/Albums';
 import Homepage from './homepage/Homepage';
+import axios from 'axios';
 
 
 export class App extends React.Component {
 
   state = {
-    albums : [
-      {
-        id : 1,
-        title : "Valar Morghulis"
-      },
-      {
-        id : 2,
-        title : "All Men Must Die"
-      },
-      {
-        id : 3,
-        title : "Vala Dohaeris"
-      }
-    ]
+    albums: []
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => {
+        this.setState({
+          albums: response.data
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
@@ -44,7 +44,7 @@ export class App extends React.Component {
 
           <Route path="/" exact component={Homepage} />
           <Route path="/about/" component={About} />
-          <Route path="/albums/" render = {() => <Albums albums = {this.state.albums}></Albums>} />
+          <Route path="/albums/" render={() => <Albums albums={this.state.albums}></Albums>} />
         </div>
       </Router>
     );
